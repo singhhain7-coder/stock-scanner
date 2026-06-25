c46127e3ee5d61bde49a5b72babd819fb0c1c1ed
singhhain7-coder <singhhain7@gmail.com>
2026-06-26 01:03:47 +0530

    Add /scanner endpoint to server

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-
-const app = express();
-app.use(cors());
-
-app.get("/", (req, res) => {
-  res.send("Stock Scanner API Running");
-});
-
-const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-  console.log("Server Started");
+app.get("/scanner", (req, res) => {
+  res.json({ status: "ok" });
 });
